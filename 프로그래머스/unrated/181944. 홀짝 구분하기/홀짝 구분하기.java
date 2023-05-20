c56a96efd6d5e9a76d84c7f1f222231f1ed48ae0
dongmin132 <114303854+dongmin132@users.noreply.github.com>
2023-05-20 17:03:32 +0900

    [level 0] Title: 종이 자르기, Time: 0.03 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181944. 홀짝 구분하기/홀짝 구분하기.java
+++ 프로그래머스/unrated/181944. 홀짝 구분하기/홀짝 구분하기.java
@@ -2,11 +2,11 @@ import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        Scanner sc = new Scanner(System.in);        //입력값
+        int n = sc.nextInt();                       //사용자로부터 입력된 정수 값을 읽어옴
         
         if(n%2==0)
-            System.out.println(n +" is even");
+            System.out.println(n +" is even");      
         else
             System.out.println(n +" is odd");
     }
