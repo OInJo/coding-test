3beaf1584b955926896947cc1f6918b0e2af0215
dongmin132 <114303854+dongmin132@users.noreply.github.com>
2023-05-20 13:40:28 +0900

    [level 0] Title: OX퀴즈, Time: 0.10 ms, Memory: 77.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.java
@@ -0,0 +1,24 @@
+import static java.lang.Integer.parseInt;
+
+class Solution {
+    public String[] solution(String[] quiz) {
+
+        String [] answer = new String[quiz.length];
+        int result;
+        for (int i = 0; i < quiz.length; i++) {
+            String [] arr = quiz[i].split(" ");
+            if(arr[1].equals("+")) {
+                if (Integer.parseInt(arr[0]) + Integer.parseInt(arr[2]) == Integer.parseInt(arr[4]))
+                    answer[i] = "O";
+                else
+                    answer[i]="X";
+            } else if(arr[1].equals("-")){
+                if (Integer.parseInt(arr[0]) - Integer.parseInt(arr[2]) == Integer.parseInt(arr[4]))
+                    answer[i] = "O";
+                else
+                    answer[i]="X";
+            }
+        }
+        return answer;
+    }
+}
