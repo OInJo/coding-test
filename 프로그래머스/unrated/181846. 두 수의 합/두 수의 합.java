302a59d37d0d29343fe9fef51d219e10ff4e0a56
dongmin132 <114303854+dongmin132@users.noreply.github.com>
2023-05-20 19:30:35 +0900

    [unrated] Title: 두 수의 합, Time: 225.84 ms, Memory: 111 MB -BaekjoonHub

--- 프로그래머스/unrated/181846. 두 수의 합/두 수의 합.java
+++ 프로그래머스/unrated/181846. 두 수의 합/두 수의 합.java
@@ -1,12 +1,12 @@
 import java.math.BigInteger;
 
 class Solution {
-    public String solution(String a, String b) {        
-        BigInteger intA= new BigInteger(a);         //주어진 문자열 a를 기반으로 BigInteger 객체 intA를 생성
-        BigInteger intB= new BigInteger(b);         //주어진 문자열 b를 기반으로 BigInteger 객체 intB를 생성    
+    public String solution(String a, String b) {
+        BigInteger intA= new BigInteger(a);
+        BigInteger intB= new BigInteger(b);
 
-        BigInteger answer = intA.add(intB);         //BigInteger class에 add() 메서드를 사용해 두 값을 더함
+        BigInteger answer = intA.add(intB);
 
-        return answer.toString();                   //toString() 메서드를 사용해 BigInteger 객체를 문자열로 변환해서 반환
+        return answer.toString();
     }
 }
