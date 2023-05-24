2317434a50123692fb8eb1ccc44c031158818d66
minwook <zkvn1103@naver.com>
2023-05-24 09:05:50 +0900

    2주차(3,4,5)

--- minwook/2주차/평행.java
+++ minwook/2주차/평행.java
@@ -8,8 +8,8 @@ class Solution {
     }
     
     private int checkSlopeEquality(int[][] dots, int a, int b, int c, int d) { // 기울기가 같은지 확인하는 함수
-            double numerator = (double)(dots[b][1] - dots[a][1]) / (dots[b][0] - dots[a][0]); // 기울기가 소수점일 경우를 대비하여 double로 형변환
-            double denominator = (double)(dots[d][1] - dots[c][1]) / (dots[d][0] - dots[c][0]);
-        return (numerator == denominator) ? 1 : 0; // 기울기가 같은 경우 1 아니면 0을 반환
+            double slope1 = (double)(dots[b][1] - dots[a][1]) / (dots[b][0] - dots[a][0]); // 기울기가 소수점일 경우를 대비하여 double로 형변환
+            double slope2 = (double)(dots[d][1] - dots[c][1]) / (dots[d][0] - dots[c][0]);
+        return (slope1 == slope2) ? 1 : 0; // 기울기가 같은 경우 1 아니면 0을 반환
     }
 }
